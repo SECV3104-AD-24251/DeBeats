6779800e8e78a2c089b33ff8f905db041148cbfe
izzahRashid <izzahrashid1307@gmail.com>
2024-12-28 15:17:22 +0800

    Update lecturer
    seeder, migration, ui

--- DeBeats/database/migrations/2024_12_22_152318_create_lecturer_table.php
+++ DeBeats/database/migrations/2024_12_22_152318_create_lecturer_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('lecturer', function (Blueprint $table) {
             $table->id(); // Primary key
             $table->string('UTMID');
+            $table->string('image_path')->nullable(); // Column for image path
             $table->string('name'); // Column for lecturer name
             $table->string('course_code');  // Column for course code
             $table->timestamps(); // Created at and Updated at columns
--- DeBeats/database/seeders/LecturerSeeder.php
+++ DeBeats/database/seeders/LecturerSeeder.php
@@ -13,21 +13,21 @@ class LecturerSeeder extends Seeder
     public function run(): void
     {
         $lecturer = [
-            ['UTMID' => 'shukor', 'name' => 'Dr. Mohamad Shukor bin Talib', 'course_code' => 'SECI1013', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'alif', 'name' => 'Dr. Alif Ridzuan bin Khairuddin', 'course_code' => 'SECJ1013', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'iqbal', 'name' => 'Ts. Dr. Muhammad Iqbal Tariq bin Idris', 'course_code' => 'SECP1513', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'foad', 'name' => 'Dr. Mohd Fo\'ad bin Rohani', 'course_code' => 'SECR1013', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'hazinah', 'name' => 'Ts. Ms. Hazinah binti Kutty Mammi', 'course_code' => 'SECR1213', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'izyan', 'name' => 'Dr. Izyan Izzati binti Kamsani', 'course_code' => 'SECD2524', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'jamilah', 'name' => 'Dr. Jamilah binti Mahmood', 'course_code' => 'SECJ2203', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'pang', 'name' => 'Dr. Pang Yee Yong', 'course_code' => 'SECJ2013', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'zuraifah', 'name' => 'Dr. Nur Zuraifah Syazrah binti Othman', 'course_code' => 'SECV2113', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'shafaatunnur', 'name' => 'Dr. Shafaatunnur binti Hassan', 'course_code' => 'SECJ3553', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'shahida', 'name' => 'Assoc. Prof. Dr. Shahida binti Sulaiman', 'course_code' => 'SECV3104', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'sah', 'name' => 'Dr. Md. Sah bin Hj. Salam', 'course_code' => 'SECV3213', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'norhaida', 'name' => 'Dr. Norhaida binti Mohd Suaib', 'course_code' => 'SECV3113', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'jumail', 'name' => 'Dr. Jumail bin Taliba', 'course_code' => 'SECJ3623', 'created_at' => now(), 'updated_at' => now()],
-            ['UTMID' => 'aida', 'name' => 'Dr. Aida binti Ali', 'course_code' => 'SECV4134', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'shukor', 'image_path' => 'images/shukor.jpg', 'name' => 'Dr. Mohamad Shukor bin Talib', 'course_code' => 'SECI1013', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'alif', 'image_path' => 'images/alif.jpg', 'name' => 'Dr. Alif Ridzuan bin Khairuddin', 'course_code' => 'SECJ1013', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'iqbal', 'image_path' => 'images/iqbal.jpg', 'name' => 'Ts. Dr. Muhammad Iqbal Tariq bin Idris', 'course_code' => 'SECP1513', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'foad', 'image_path' => 'images/foad.jpg', 'name' => 'Dr. Mohd Fo\'ad bin Rohani', 'course_code' => 'SECR1013', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'hazinah', 'image_path' => 'images/hazinah.jpg', 'name' => 'Ts. Ms. Hazinah binti Kutty Mammi', 'course_code' => 'SECR1213', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'izyan', 'image_path' => 'images/izyan.jpg', 'name' => 'Dr. Izyan Izzati binti Kamsani', 'course_code' => 'SECD2524', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'jamilah', 'image_path' => 'images/jamilah.jpg', 'name' => 'Dr. Jamilah binti Mahmood', 'course_code' => 'SECJ2203', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'pang', 'image_path' => 'images/pang.jpg', 'name' => 'Dr. Pang Yee Yong', 'course_code' => 'SECJ2013', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'zuraifah', 'image_path' => 'images/zuraifah.jpg', 'name' => 'Dr. Nur Zuraifah Syazrah binti Othman', 'course_code' => 'SECV2113', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'shafaatunnur', 'image_path' => 'images/shafaatunnur.jpg', 'name' => 'Dr. Shafaatunnur binti Hassan', 'course_code' => 'SECJ3553', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'shahida', 'image_path' => 'images/shahida.jpg', 'name' => 'Assoc. Prof. Dr. Shahida binti Sulaiman', 'course_code' => 'SECV3104', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'sah', 'image_path' => 'images/sah.jpg', 'name' => 'Dr. Md. Sah bin Hj. Salam', 'course_code' => 'SECV3213', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'norhaida', 'image_path' => 'images/norhaida.jpg', 'name' => 'Dr. Norhaida binti Mohd Suaib', 'course_code' => 'SECV3113', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'jumail', 'image_path' => 'images/jumail.jpg', 'name' => 'Dr. Jumail bin Taliba', 'course_code' => 'SECJ3623', 'created_at' => now(), 'updated_at' => now()],
+            ['UTMID' => 'aida', 'image_path' => 'images/aida.jpg', 'name' => 'Dr. Aida binti Ali', 'course_code' => 'SECV4134', 'created_at' => now(), 'updated_at' => now()],
         ];
         
 
